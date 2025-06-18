014309d6816b5e1235c2ed03e82b6ac67199a7b4
josura <giorgiolocicero@hotmail.com>
2025-06-19 00:01:58 +0200

    chore: added some TODOs for the implementation

--- transcribe.py
+++ transcribe.py
@@ -1,7 +1,11 @@
 import whisper
 
+# TODO: Read the audio files from the "dialogues_example" directory
+
 # Load the Whisper model and transcribe an audio file
 filename = "dialogues_example/audio-1.mp3"
 model = whisper.load_model("turbo")
 result = model.transcribe("audio.mp3")
 print(result["text"])
+
+#TODO: Save the transcribed texts to some files inside a "transcriptions" directory
